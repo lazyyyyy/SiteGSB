af89502f2099bd0ffab4cf02afe799fdd357ca7d
Florian Spadaro <spadaro.florian@outlook.fr>
2017-03-06 16:58:54 +0100

    ajout de la fonction javascript pour afficher la liste des praticiens dans le choicebox de compterendu.html

--- js/compterendu.js
+++ js/compterendu.js
@@ -1,1 +1,31 @@
 var form = document.querySelector("form");
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    
+    var data = new FormData(form);
+    
+    
+    ajaxPost("../api/addCompteRendu.php", data, function(){}, true);
+});
+
+var selectPraticien = document.getElementById("menu_destination");
+
+var data = new FormData();
+data.append("nom_praticien", "");
+
+ajaxPost("../api/getPraticienByName.php", data, function(reponse){
+    var praticiens = JSON.parse(reponse);
+    
+    praticiens.forEach(function(praticien){
+        var optionElt = document.createElement("option");
+        var nom = praticien.nom.toUpperCase();
+        var prenom = praticien.prenom.toLowerCase();
+        optionElt.textContent = nom + " " + prenom.charAt(0).toUpperCase() + prenom.slice(1);
+        
+        var optionName = praticien.nom.toLowerCase() + praticien.id;
+        optionElt.value = optionName;
+        
+        selectPraticien.appendChild(optionElt);
+    });
+}, false);
