f36403167fae52147bca4490363e6b4659c01c7c
Florian Spadaro <spadaro.florian@outlook.fr>
2017-04-23 18:11:24 +0200

    Résolution du problème pour récupérer l'id de l'utilisateur lors de la connexion

--- js/login.php
+++ js/login.php
@@ -1,16 +1,15 @@
 <?php
     session_start();
     include("../../api/fonctions.php");
-    $user = connexion($_POST["login"], $_POST["mdp"]);
-    echo strlen($user);
-	if(strlen($user) == 41)
+    $user = json_decode(connexion($_POST["login"], $_POST["mdp"]));
+	if($user->id == null)
 	{
 		header('Location: ../index.php?erreur=1');
 		exit();
 	}
 	else
 	{
-		$_SESSION["user_id_json"] = $user;
+		$_SESSION["user_id_json"] = $user->id;
 		header('Location: ../home.php');
 		exit();
 	}
